e5d65776a5ea40eae320e443f7c68a6723be7753
OJ <onkarjoshi296@gmail.com>
2024-10-19 00:41:31 +0530

    addition

--- backend/routes/listUser.js
+++ backend/routes/listUser.js
@@ -4,23 +4,21 @@ const User = require("../model/createUser");
 
 router.get("/list_users", async (req, res) => {
   try {
-    // Check if the request is expecting HTML
-    if (req.headers.accept && req.headers.accept.includes("text/html")) {
-      // Send HTML response when accessed via a browser
-      res.status(200).send("<html><body><h1>Reached this point</h1></body></html>");
-    } else {
-      // Handle API request (JSON response)
-      const uniqueUsers = await User.aggregate([
-        {
-          $group: {
-            _id: "$user",
-            user: { $first: "$user" },
-          },
-        },
-      ]);
+    res
+      .status(200)
+      .send("<html><body><h1>Reached this point</h1></body></html>");
 
-      res.status(200).json(uniqueUsers);
-    }
+    // Handle API request (JSON response)
+    const uniqueUsers = await User.aggregate([
+      {
+        $group: {
+          _id: "$user",
+          user: { $first: "$user" },
+        },
+      },
+    ]);
+
+    res.status(200).json(uniqueUsers);
   } catch (error) {
     console.error("Error fetching unique users:", error);
     res.status(500).json({
