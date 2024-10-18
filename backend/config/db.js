849486b57d4d81da1d227efaf4e626da321876f6
OJ <onkarjoshi296@gmail.com>
2024-10-19 02:18:18 +0530

    mongodb alive

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const connectDB = async (mongoUrl) => {
   try {
     await mongoose.connect(mongoUrl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      socketTimeoutMS: 45000,  // Increase socket timeout to 45 seconds
+      keepAlive: true,
+      keepAliveInitialDelay: 300000,  // Set keep-alive to 5 minutes
     });
+    
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("MongoDB connection error:", err);
