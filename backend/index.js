bfbd75e162871fcdcf6e277d82a2dca84f632873
OJ <onkarjoshi296@gmail.com>
2024-10-26 09:44:33 +0530

    changed the ui route

--- backend/index.js
+++ backend/index.js
@@ -20,23 +20,32 @@ const mongoUrl = process.env.MONGO_URL;
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "https://chatbot-ui-two-bay.vercel.app",
+    origin: "https://chatbot-en.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
-    maxAge: 86400
+    maxAge: 86400,
   })
 );
 
 app.get("/", async (req, res) => {
   const connectionStatus = await connectDB(mongoUrl);
-  
+
   if (connectionStatus.success) {
-    res.send("<html><body><h3>Server is up and running!</h3><p>" + connectionStatus.message + "</p></body></html>");
+    res.send(
+      "<html><body><h3>Server is up and running!</h3><p>" +
+        connectionStatus.message +
+        "</p></body></html>"
+    );
   } else {
-    res.send("<html><body><h3>Server failed to connect!</h3><p>" + connectionStatus.message + " mongoUrl: " + mongoUrl+"</p></body></html>");
+    res.send(
+      "<html><body><h3>Server failed to connect!</h3><p>" +
+        connectionStatus.message +
+        " mongoUrl: " +
+        mongoUrl +
+        "</p></body></html>"
+    );
   }
 });
-
 
 app.use("/api", userRoutes);
 app.use("/api", createResponse);
