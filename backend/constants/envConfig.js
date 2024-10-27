5bc4d319c33ef42c779cd6cddb897643e17267a5
OJ <onkarjoshi296@gmail.com>
2024-10-27 22:50:27 +0530

    modifed index conf

--- backend/constants/envConfig.js
+++ backend/constants/envConfig.js
@@ -1,5 +1,5 @@
-const baseURL = process.env.NODE_ENV === "prod" 
-  ? "https://chatbot-en.vercel.app" 
-  : "http://localhost:3000";
+const baseURL = process.env.NODE_ENV === "local" 
+  ? "http://localhost:3000" 
+  : "https://chatbot-en.vercel.app";
 
 module.exports = { baseURL };
